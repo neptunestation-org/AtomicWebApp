9e832a8b379981d7c7cad2fc20d986e9b0c2a6c5
NeptuneStation.org <neptunestationorg@david-desktop>
2022-12-04 15:26:20 -0800

    Fixed a bunch of stuff

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,5 @@
 ;;; Directory Local Variables
 ;;; See Info node `(emacs) Directory Variables' for more information.
 
-((java-mode
-  (compile-command . (concat "cd " (let ((l (dir-locals-find-file (or (buffer-file-name) default-directory)))) (if (listp l) (car l) l)) " && mvn -B compile"))))
+((nil
+  (compile-command . "cd `git rev-parse --show-toplevel` && mvn verify")))
